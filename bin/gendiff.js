77f508b5fbce381181730880756dae8962b00817
Largebro <carpowaleks3@gmail.com>
2021-11-04 02:42:59 +0400

    add gendiff

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,4 +4,4 @@ import program from 'commander';
 program
   .version('0.1')  
   .description('Compares two configuration files and shows a difference.') 
-  program.parse(); //
+  program.parse(); //d
